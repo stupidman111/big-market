64653cc2eca5974c50d74cbbcdebfc360f4abb92
zy <2456306700@qq.com>
2024-06-24 21:28:59 +0800

    feat: dev-ops

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# xfg-frame-archetype - DDD 脚手架
+# big-market
 
 
-md5 scaffold-lite-1.0.pom > scaffold-lite-1.0.pom.md5
-sha1sum scaffold-lite-1.0.pom > scaffold-lite-1.0.pom.sha1
--- big-market-app/build.sh
+++ big-market-app/build.sh
@@ -1,6 +1,17 @@
 
 # 普通镜像构建，随系统版本构建 amd/arm
-docker build -t system/big-market-app:1.0-SNAPSHOT -f ./Dockerfile .
+# docker build -t system/big-market-app:1.0-SNAPSHOT -f ./Dockerfile .
 
 # 兼容 amd、arm 构建镜像
-# docker buildx build --load --platform liunx/amd64,linux/arm64 -t xiaofuge/xfg-frame-archetype-app:1.0 -f ./Dockerfile . --push
+# docker buildx build --load --platform liunx/amd64,linux/arm64 -t zy/big-market-app:1.0-SNAPSHOT -f ./Dockerfile . --push
+# docker buildx build --platform liunx/amd64,linux/arm64 -t zy/big-market-app:1.0-SNAPSHOT -f ./Dockerfile . --push
+
+#!/bin/bash
+# 创建并使用新的 builder
+docker buildx create --name mybuilder --use --driver docker-container
+# 启动 builder
+docker buildx inspect mybuilder --bootstrap
+# 运行多平台构建并推送镜像
+docker buildx build --platform linux/amd64,linux/arm64 -t zysdaoke/big-market-app-1.0 -f ./Dockerfile . --push
+# 删除 builder（可选）
+docker buildx rm mybuilder
